79d345df3b5e71f65d7cc72fedb3e87fe508349c
Kishore. V <157293196+vk22006@users.noreply.github.com>
2025-11-15 12:30:21 +0530

    Reformat background setting code

--- 1_Home.py
+++ 1_Home.py
@@ -1,3 +1,26 @@
+'''**********************************************************************************
+    SPDX-License-Identifier: MIT
+    Copyright (c) 2025 Kishore. V
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+**********************************************************************************'''
+
 import streamlit as st
 import base64
 
@@ -29,18 +52,6 @@ st.set_page_config(
 )
 
 st.logo("assets/logo.png")
-
-# page_bg_img = '''
-# <style>
-# body {
-# background-image: url('https://wallpapers.com/images/high/modern-data-center-infrastructure-azgwkzrjo2ohl4gd.webp');
-# background-size: cover;
-# opacity: 0.8;
-# }
-# </style>
-# '''
-
-# st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.title("❄️ Predictive Cooling Optimizer for Data Centers")
 st.markdown("### Temperature-Aware Chiller Scheduling to Cut Energy Use")
@@ -81,3 +92,4 @@ st.info(
     * **Model Details**: Learn more about the 46 features  and testing methodology used.
     """
 )
+
